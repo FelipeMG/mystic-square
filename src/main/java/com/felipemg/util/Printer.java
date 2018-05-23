8d229d87fc01463d9f40f8fe4560edcecda5db19
FelipeMG <felipemontana@gmail.com>
2018-05-22 22:36:36 -0500

    added Printer class

---
+++ src/main/java/com/felipemg/util/Printer.java
@@ -0,0 +1,11 @@
+package com.felipemg.util;
+
+public final class Printer {
+
+    public static final String NEW_LINE = String.format("%n");
+
+    private Printer(){
+        throw new AssertionError("This class should not be instantiated");
+    }
+
+}
