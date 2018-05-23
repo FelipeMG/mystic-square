0ce023d056585488647981386e29db5ffde81be1
FelipeMG <felipemontana@gmail.com>
2018-05-22 19:28:19 -0500

    add Position tests

--- src/test/java/com/felipemg/util/PositionTest.java
+++ src/test/java/com/felipemg/util/PositionTest.java
@@ -1,6 +1,22 @@
 package com.felipemg.util;
 
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
 public class PositionTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void positionOf0ShouldReturnIllegalArgumentException(){
+        assertThat(Position.of(0,0),is(Position.of(1,1)));
+    }
+
+    @Test()
+    public void positionOfXequals1AndYequals1IsNotNull(){
+        assertThat(Position.of(1,1),is(notNullValue()));
+    }
 
 
 }
