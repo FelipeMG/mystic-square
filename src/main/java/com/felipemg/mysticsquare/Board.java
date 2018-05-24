9338823bc1d7d859abf917eeba70545a1896dcc3
FelipeMG <felipemontana@gmail.com>
2018-05-23 22:25:45 -0500

    improve readability

--- src/main/java/com/felipemg/mysticsquare/Board.java
+++ src/main/java/com/felipemg/mysticsquare/Board.java
@@ -1,6 +1,7 @@
 package com.felipemg.mysticsquare;
 
 import com.felipemg.util.Position;
+import com.felipemg.util.Printer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,17 +9,21 @@ import java.util.Map;
 public final class Board {
 
     private Map<String, Position> tiles;
+    private int size;
 
     private Board(int size){
+        this.size = size;
         this.tiles = initializeBoard(size);
     }
 
     private Map<String, Position> initializeBoard(int size){
-        Map<String,Position> tiles = new HashMap<String, Position>(size * size);
+
+        Integer numberOfTiles = (size * size) -1 ;
+        Map<String,Position> tiles = new HashMap<String, Position>(numberOfTiles);
         for (int tile = 1; tile < size; tile++) {
-            int x = (int) Math.ceil(tile /4.0);
-            int y = tile % 4;
-            tiles.put(Integer.toString(tile),Position.of(x,y));
+            int xIndex = (int) Math.ceil(tile * 1.0 /size);
+            int yIndex = tile % size;
+            tiles.put(Integer.toString(tile),Position.of(xIndex,yIndex));
         }
         tiles.put("",Position.of(size,size));
         return tiles;
@@ -35,4 +40,5 @@ public final class Board {
                 "tiles=" + tiles +
                 '}';
     }
+
 }
