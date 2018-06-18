f5c0be4dbf6fee5808c4f6e8f0bd34f4c6e4a547
FelipeMG <felipemontana@gmail.com>
2018-06-17 23:09:06 -0500

    fixed Position tests

--- src/test/java/com/felipemg/util/PositionTest.java
+++ src/test/java/com/felipemg/util/PositionTest.java
@@ -9,8 +9,13 @@ import static org.hamcrest.CoreMatchers.*;
 public class PositionTest {
 
     @Test(expected = IllegalArgumentException.class)
-    public void positionOf0ShouldReturnIllegalArgumentException(){
-        assertThat(Position.of(0,0),is(Position.of(1,1)));
+    public void positionOfNegativeNumbersShouldReturnIllegalArgumentException(){
+        Position.of(-1,1);
+    }
+
+    @Test()
+    public void positionOfXequals0AndYequals0IsNotNull(){
+        assertThat(Position.of(0,0),is(notNullValue()));
     }
 
     @Test()
@@ -18,5 +23,9 @@ public class PositionTest {
         assertThat(Position.of(1,1),is(notNullValue()));
     }
 
+    @Test()
+    public void positionOfXequals0AndYequals0IsNotEqualsTopositionOfXequals1AndYequals0(){
+        assertThat(Position.of(0,0).toString(),is(not(Position.of(1,0).toString())));
+    }
 
 }
